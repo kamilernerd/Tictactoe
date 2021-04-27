96a18dbf1f33f54b97bb17fa0c80d93363f0b11b
Kamil <kamil@indicosys.com>
2021-04-27 17:18:45 +0200

    Add doc comments and improve ServiceAPI

--- app/src/main/java/com/kamil/tictactoe/api/ServiceAPI.kt
+++ app/src/main/java/com/kamil/tictactoe/api/ServiceAPI.kt
@@ -27,6 +27,30 @@ object ServiceAPI {
     private const val JOIN_GAME = "$BASE_URI/game/join"
     private const val CREATE_GAME = "$BASE_URI/game"
 
+    /**
+     * Function to rebuild json string
+     * Somehow 2d state array is converted to a string of 2d array and makes gson go nuts
+     * @param response [String] Server response
+     * @return [String] New json string
+     */
+    private fun rebuildJsonString(response: String): String {
+        val jsonResponse = JSONObject(response)
+        val state = jsonResponse.getString("state")
+        val players = jsonResponse.getString("players")
+        val gameId = jsonResponse.getString("gameId")
+
+        // Somehow 2d state array is converted to a string of 2d array and makes gson go nuts
+        return "{\"players\": $players, \"gameId\": \"$gameId\", \"state\": $state}"
+    }
+
+    /**
+     * Pull game data
+     *
+     * @param requestQueue [RequestQueue1] Volley request queue
+     * @param currentState [GameState] Current local game state
+     * @param state [StateList] 3x3 matrix of local game state
+     * @param callback [UpdateGameCallback] Callback when updating game data went successfully
+     */
     fun updateGame(requestQueue: RequestQueue1, currentState: GameState, state: StateList, callback: UpdateGameCallback) {
         val body = JSONObject()
         body.put("gameId", currentState.gameId)
@@ -34,17 +58,7 @@ object ServiceAPI {
         body.put("state", state)
 
         val request = object : JsonObjectRequest(Method.POST, "${BASE_URI}/game/${currentState.gameId}/update", body, Response.Listener { response ->
-                Log.println(Log.VERBOSE, "GameAPI UPDATE", response.toString())
-
-                val jsonResponse = JSONObject("$response")
-                val state = jsonResponse.getString("state")
-                val players = jsonResponse.getString("players")
-                val gameId = jsonResponse.getString("gameId")
-
-                // Somehow 2d state array is converted to a string of 2d array and makes gson go nuts
-                val customJsonStringHack = "{\"players\": $players, \"gameId\": \"$gameId\", \"state\": $state}"
-
-                callback(Gson().fromJson(customJsonStringHack, GameState::class.java))
+                callback(Gson().fromJson(rebuildJsonString(response.toString()), GameState::class.java))
             },
             Response.ErrorListener { error ->
                 Log.println(Log.VERBOSE, "GameAPI response error", error.toString())
@@ -59,19 +73,16 @@ object ServiceAPI {
         requestQueue.add(request)
     }
 
+    /**
+     * Pull game data
+     *
+     * @param requestQueue [RequestQueue1] Volley request queue
+     * @param gameId [StateList] Game id to join
+     * @param callback [PollGameCallback] Callback when pulling game data went successfully
+     */
     fun pollGame(requestQueue: RequestQueue1, gameId: String, callback: PollGameCallback) {
         val request = object : JsonObjectRequest(Method.GET, "${BASE_URI}/game/$gameId/poll", null, Response.Listener { response ->
-                Log.println(Log.VERBOSE, "GameAPI POLL", response.toString())
-
-                val jsonResponse = JSONObject("$response")
-                val state = jsonResponse.getString("state")
-                val players = jsonResponse.getString("players")
-                val gameId = jsonResponse.getString("gameId")
-
-                // Somehow 2d state array is converted to a string of 2d array and makes gson go nuts
-                val customJsonStringHack = "{\"players\": $players, \"gameId\": \"$gameId\", \"state\": $state}"
-                val decodedJson = Gson().fromJson(customJsonStringHack, GameState::class.java)
-                callback(decodedJson)
+                callback(Gson().fromJson(rebuildJsonString(response.toString()), GameState::class.java))
             },
             Response.ErrorListener { error ->
                 Log.println(Log.VERBOSE, "GameAPI response error", error.toString())
@@ -86,23 +97,21 @@ object ServiceAPI {
         requestQueue.add(request)
     }
 
+    /**
+     * Join existing game
+     *
+     * @param requestQueue [RequestQueue1] Volley request queue
+     * @param gameId [StateList] Game id to join
+     * @param playerName [String] Game creator name
+     * @param callback [JoinGameCallback] Callback when user joined successfully
+     */
     fun joinGame(requestQueue: RequestQueue1, gameId: String, playerName: String, callback: JoinGameCallback) {
         val body = JSONObject()
         body.put("gameId", gameId)
         body.put("player", playerName)
 
         val request = object : JsonObjectRequest(Method.POST, "${BASE_URI}/game/$gameId/join", body, Response.Listener { response ->
-                Log.println(Log.VERBOSE, "GameAPI JOIN", response.toString())
-
-                val jsonResponse = JSONObject("$response")
-                val state = jsonResponse.getString("state")
-                val players = jsonResponse.getString("players")
-                val gameId = jsonResponse.getString("gameId")
-
-                // Somehow 2d state array is converted to a string of 2d array and makes gson go nuts
-                val customJsonStringHack = "{\"players\": $players, \"gameId\": \"$gameId\", \"state\": $state}"
-
-                callback(gameId, Gson().fromJson(customJsonStringHack, GameState::class.java))
+                callback(gameId, Gson().fromJson(rebuildJsonString(response.toString()), GameState::class.java))
             },
             Response.ErrorListener { error ->
                 Log.println(Log.VERBOSE, "GameAPI response error", error.toString())
@@ -117,23 +126,21 @@ object ServiceAPI {
         requestQueue.add(request)
     }
 
+    /**
+     * Create new game
+     *
+     * @param requestQueue [RequestQueue1] Volley request queue
+     * @param playerName [String] Game creator name
+     * @param matchState [StateList] Initial game state
+     * @param callback [CreateGameCallback] Callback when game has been created successfully
+     */
     fun createGame(requestQueue: RequestQueue1, playerName: String, matchState: StateList, callback: CreateGameCallback) {
         val body = JSONObject()
         body.put("player", playerName)
         body.put("state", matchState)
 
-        Log.println(Log.VERBOSE, "GameAPI CREATE", CREATE_GAME)
-
-        val request = object : JsonObjectRequest(Request.Method.POST, CREATE_GAME, body, Response.Listener { response ->
-            val jsonResponse = JSONObject("$response")
-            val state = jsonResponse.getString("state")
-            val players = jsonResponse.getString("players")
-            val gameId = jsonResponse.getString("gameId")
-
-            // Somehow 2d state array is converted to a string of 2d array and makes gson go nuts
-            val customJsonStringHack = "{\"players\": $players, \"gameId\": \"$gameId\", \"state\": $state}"
-
-            callback(Gson().fromJson(customJsonStringHack, GameState::class.java))
+        val request = object : JsonObjectRequest(Method.POST, CREATE_GAME, body, Response.Listener { response ->
+            callback(Gson().fromJson(rebuildJsonString(response.toString()), GameState::class.java))
         }, Response.ErrorListener { error ->
             Log.println(Log.VERBOSE, "GameAPI response error", error.toString())
         }) {
@@ -160,7 +167,7 @@ object ServiceAPI {
         val timer = Timer()
         val doAsynchronousTask: TimerTask = object : TimerTask() {
             override fun run() {
-                handler.post(Runnable {
+                handler.post {
                     try {
                         pollGame(Volley.newRequestQueue(context), gameId) {
                             callback(it, timer)
@@ -168,7 +175,7 @@ object ServiceAPI {
                     } catch (e: Exception) {
                         // TODO Auto-generated catch block
                     }
-                })
+                }
             }
         }
         timer.schedule(doAsynchronousTask, 0, 5000) //execute in every 50000 ms
